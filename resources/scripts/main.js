b4782ca9a652291338acdd6d3e4a8259affcb4d3
ronnieskansing <rskansing@gmail.com>
2014-10-23 21:05:17 +0200

    update teh apigen

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,13 +1,17 @@
 
+window.addEventListener('onload', function() {
+    TogetherJS(this);
+})
+
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");
+//editor.getSession().setMode("ace/mode/javascript");
 editor.getSession().setTabSize(4);
 editor.getSession().setUseSoftTabs(true);
 
 editor.commands.addCommand({
     name: 'phpHighlighting',
-    bindKey: {win: 'Alt-Shift-P',  mac: 'Shift-Command-M'},
+    bindKey: {win: 'Alt-Shift-1',  mac: 'Shift-Command-M1'},
     exec: function(editor) {
         editor.getSession().setMode("ace/mode/php");
     },
@@ -16,17 +20,19 @@ editor.commands.addCommand({
 
 editor.commands.addCommand({
     name: 'jsHighlighting',
-    bindKey: {win: 'Alt-Shift-J',  mac: 'Shift-Command-J'},
+    bindKey: {win: 'Alt-Shift-2',  mac: 'Shift-Command-2'},
     exec: function(editor) {
+        console.log('Javascript!');
         editor.getSession().setMode("ace/mode/javascript");
     },
     readOnly: true // false if this command should not apply in readOnly mode
 });
 
 editor.commands.addCommand({
-    name: 'jsHighlighting',
-    bindKey: {win: 'Alt-Shift-C',  mac: 'Shift-Command-C'},
+    name: 'cHighlighting',
+    bindKey: {win: 'Alt-Shift-3',  mac: 'Shift-Command-3'},
     exec: function(editor) {
+
         editor.getSession().setMode("ace/mode/c");
     },
     readOnly: true // false if this command should not apply in readOnly mode
